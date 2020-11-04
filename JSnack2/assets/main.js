c33c818f1c743d5c158b3ea83744fab5b9d3ebd2
Rocco <rocco.chiarella.rc@gmail,com>
2020-11-04 16:46:54 +0100

    JSnack2

--- JSnack2/assets/main.js
+++ JSnack2/assets/main.js
@@ -2,4 +2,20 @@ $(document).ready(function() {
 
 const array = ['a', 'b', 'c', 'd', 'e', 'f'];
 
+var numero1 = parseInt(prompt("Inserisci il primo numero"));
+
+var numero2 = parseInt(prompt("Inserisci il secondo numero"));
+
+var valoriCompresi = [];
+
+array.forEach((elementi, indice) => {
+    if ((indice >= numero1) && (indice <= numero2)) {
+        valoriCompresi.push(elementi);
+    }
+});
+
+for (var i = 0; i < valoriCompresi.length; i++) {
+    $('.elementi').append('<li>'+valoriCompresi[i]+'</li>');
+}
+
 });
