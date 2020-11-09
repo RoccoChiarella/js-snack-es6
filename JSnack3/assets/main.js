3eb1fbf65825cc54ac903944b44e35f6dbb17af6
Rocco <rocco.chiarella.rc@gmail,com>
2020-11-09 16:21:40 +0100

    JSnack 3

--- JSnack3/assets/main.js
+++ JSnack3/assets/main.js
@@ -2,11 +2,7 @@ $(document).ready(function() {
 
 const array = ['a', 'b', 'c', 'd', 'e', 'f'];
 
-var min = parseInt(prompt("Inserisci un numero compreso tra 0 e 5"));
-
-var max = parseInt(prompt("Inserisci un numero compreso tra 0 e 5"));
-
-var valoriCompresi = [];
+var arrayClone = [...array];
 
 array.forEach((elementi, indice) => {
     if ((indice >= min) && (indice <= max)) {
